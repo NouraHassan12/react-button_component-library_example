6f4a7130ea354854cb1427e9ec78e534504c81d6
Nourhassann <nourhassan132@gmail.com>
2022-12-28 13:29:38 +0200

    first commit

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,5 +1,5 @@
 import React, { ElementType, MouseEventHandler, ReactNode } from "react";
-import { StyledButton, StyledIcon } from "./styled";
+import { StyledButton } from "./styled";
 export type ButtonType = "default" | "danger" | "ghost" | "secondary";
 
 export type ComponentSize = "default" | "large" | "small";
@@ -18,88 +18,41 @@ type HTMLButtonProps = {
   onClick?: MouseEventHandler<HTMLButtonElement>;
 } & BaseButtonProps;
 
-type HTMLAnchorProps = {
-  href?: string;
-} & BaseButtonProps;
-
 type CustomNodeProps = {
   as?: ElementType;
   to?: string;
 } & BaseButtonProps;
 
-export type ButtonProps = HTMLButtonProps & HTMLAnchorProps & CustomNodeProps;
+export type ButtonProps = HTMLButtonProps & CustomNodeProps;
 
 const Button: React.ForwardRefRenderFunction<unknown, ButtonProps> = (
-  props,
-  ref
+  props
 ) => {
   const {
     type = "default",
-    icon,
     size = "default",
     className,
     children,
-    disabled = false,
-    loading,
     onClick,
-    href,
-    as,
-    to,
   } = props;
 
   const styles = {
     innerType: type,
     size,
-    disabled,
     withText: children != null,
   };
 
-  const spinnerStyles = {
-    size: size === "large" ? 25 : size === "default" ? 20 : 15,
-    light: true,
-  };
-
-  const childrenWithIcon = !icon ? (
-    children
-  ) : (
-    <>
-      {children}
-      <StyledIcon as={icon} />
-    </>
-  );
-
-  if (as && !disabled) {
-    return (
-      <StyledButton as={as} to={to} ref={ref} className={className} {...styles}>
-        {loading ? <>Loading</> : childrenWithIcon}
-      </StyledButton>
-    );
-  }
-
-  if (href && !disabled) {
-    return (
-      <StyledButton
-        as="a"
-        href={href}
-        ref={ref as React.MutableRefObject<HTMLAnchorElement>}
-        className={className}
-        {...styles}
-      >
-        {loading ? <>Loading</> : childrenWithIcon}
-      </StyledButton>
-    );
-  }
+  const childrenWithIcon = <>{children}</>;
 
   return (
     <StyledButton
       as="button"
       type="button"
       onClick={onClick}
-      ref={ref as React.MutableRefObject<HTMLButtonElement>}
       className={className}
       {...styles}
     >
-      {loading ? <>Loading</> : childrenWithIcon}
+      {childrenWithIcon}
     </StyledButton>
   );
 };
--- src/components/button/styled.ts
+++ src/components/button/styled.ts
@@ -49,10 +49,6 @@ export const StyledButton = styled.button<StyledButtonProps>`
   display: flex;
   align-items: center;
   justify-content: center;
-  /* Add margin in case of loading or icon */
-  & > *:nth-child(1) {
-    margin-left: ${(pr) => (pr.withText ? 7 : 0)}px;
-  }
   font-size: 15px;
   border: none;
   cursor: pointer;
@@ -61,29 +57,7 @@ export const StyledButton = styled.button<StyledButtonProps>`
   height: ${(pr) => heights[pr.size]}px;
   color: ${(pr) =>
     pr.innerType === "ghost" ? typeColors["default"].regular : "#fff"};
-  ${(pr) =>
-    pr.disabled
-      ? `
-        background-color: #a6a6a6;
-        color: #5e5e5e;
-        cursor: not-allowed;
-        &:hover {
-            background-color: #a6a6a6 !important;
-            color: #5e5e5e !important;
-        }
-    `
-      : ""}
+
   border-radius: 0;
   outline: none;
-  &:focus {
-    box-shadow: 0 0 0 1px #fff,
-      0 0 0 2px ${(pr) => typeColors[pr.innerType].regular};
-  }
-  &:hover {
-    background-color: ${(pr) => typeColors[pr.innerType].hover};
-  }
-`;
-
-export const StyledIcon = styled.div`
-  height: 20px;
 `;
